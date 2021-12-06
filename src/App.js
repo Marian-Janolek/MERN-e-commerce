5ce41303273a1ec9458e5887c7d40efcc78414d7
Marian-Janolek <m.janolek@gmail.com>
2021-12-06 18:10:19 +0100

    add login and register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from './pages/Home';
+import Login from './pages/Login';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 import SingleProduct from './pages/SingleProduct';
 
 function App() {
   return (
     <div>
-      <SingleProduct />
+      <Login />
     </div>
   );
 }
